2c55b9fc4eeff7dbb1b4b0f330e15a85d41e6b70
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-08-17 20:17:19 +0200

    finish

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import "./App.css";
 import { userValidation } from "./Validation";
 
 function App() {
+  const createUser = async (event) => {
+    event.preventDefault();
+    let formData = {
+      name:event.target[0].value,
+      email:event.target[1].value,
+      password:event.target[2].value,
+    };
+    console.log(formData)
 
-const createUser = (event)=>{
-  event.preventDefault();
-  let formData={
-    name:event.target[0].value,
-    
-  }
-}
+    const isValid = await userValidation.isValid(formData);
+    console.log(isValid);
+  };
 
   return (
     <div className="App">
