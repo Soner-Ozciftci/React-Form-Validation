256b41e53a62ee3bc0c2ed47fc7fe440ad26f0fb
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-08-17 20:10:08 +0200

    target value

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import './App.css';
+import "./App.css";
+import { userValidation } from "./Validation";
 
 function App() {
 
+const createUser = (event)=>{
+  event.preventDefault();
+  let formData={
+    name:event.target[0].value,
+    
+  }
+}
+
   return (
     <div className="App">
-<form className='form'>
-<input type='text' placeholder='type your name...'/>
-<input type='email' placeholder='type your email...'/>
-<input type='password' placeholder='type your password...'/>
-<input  className='submit' type="submit" />
-</form>
+      <form className="form" onSubmit={createUser}>
+        <input type="text" placeholder="type your name..." />
+        <input type="email" placeholder="type your email..." />
+        <input type="password" placeholder="type your password..." />
+        <input className="submit" type="submit" />
+      </form>
     </div>
   );
 }
